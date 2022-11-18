a13f46619197a1804fd0b32288dcb9fb3add3bf7
Yorick-Ryu <2524964538@qq.com>
2022-11-18 15:38:02 +0800

    优化——加群方法字符串解耦

--- app/src/main/java/com/yorick/cokotools/util/Utils.kt
+++ app/src/main/java/com/yorick/cokotools/util/Utils.kt
@@ -13,6 +13,7 @@ object Utils {
     private fun openActivity(context: Context, packageName: String, activityName: String): Boolean {
         try {
             val intent = Intent()
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             context.startActivity(intent.setClassName(packageName, activityName))
         } catch (e: ActivityNotFoundException) {
             e.printStackTrace()
@@ -24,8 +25,9 @@ object Utils {
     fun joinQQGroup(context: Context, key: String): Boolean {
         val intent = Intent()
         intent.data =
-            Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26jump_from%3Dwebapi%26k%3D$key")
-        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            Uri.parse(context.resources.getString(R.string.uriStr) + key)
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面
+        // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         return try {
             context.startActivity(intent)
             true
